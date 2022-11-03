81ab5e6ae7f7e707dcec40e6ac7eed9edb9d51f6
NicolasFontana <nico.fontana12@gmail.com>
2022-11-03 11:36:04 -0300

    finished 'drum machine' project

--- Front End Development Libraries/Drum Machine/index.js
+++ Front End Development Libraries/Drum Machine/index.js
@@ -3,55 +3,55 @@ const audioClips = [
     keyCode: 81,
     keyTrigger: "Q",
     id: "Heater-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums1.mp3",
   },
   {
     keyCode: 87,
     keyTrigger: "W",
     id: "Heater-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums2.mp3",
   },
   {
     keyCode: 69,
     keyTrigger: "E",
     id: "Heater-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums3.mp3",
   },
   {
     keyCode: 65,
     keyTrigger: "A",
     id: "Heater-4",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums4.mp3",
   },
   {
     keyCode: 83,
     keyTrigger: "S",
     id: "Clap",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums5.mp3",
   },
   {
     keyCode: 68,
     keyTrigger: "D",
     id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums6.mp3",
   },
   {
     keyCode: 90,
     keyTrigger: "Z",
     id: "Kick-n'-Hat",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums7.mp3",
   },
   {
     keyCode: 88,
     keyTrigger: "X",
     id: "Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums8.mp3",
   },
   {
     keyCode: 67,
     keyTrigger: "C",
     id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    url: "https://raw.githubusercontent.com/nadktk/fcc_nadktk/master/drum-machine/sounds/drums/drums9.mp3",
   },
 ];
 
@@ -64,85 +64,123 @@ const key = {
   D: 68,
   Z: 90,
   X: 88,
-  C: 67
-}
+  C: 67,
+};
 
 const App = () => {
   const [volume, setVolume] = React.useState(1);
   const [recording, setRecording] = React.useState("");
   const [speed, setSpeed] = React.useState(1);
+  const [power, setPower] = React.useState(true)
 
   const playRecording = () => {
-    let index = 0
+    let index = 0;
     const recordArray = recording.split("");
     const interval = setInterval(() => {
       const audioTag = document.getElementById(recordArray[index]);
-      const audioButton = document.getElementById(key[recordArray[index]])
-      audioButton.classList.add("btn-warning")
-      console.log(audioButton)
+      const audioButton = document.getElementById(key[recordArray[index]]);
+      audioButton.classList.add("drum-active");
+      console.log(audioButton);
       setTimeout(() => {
-        audioButton.classList.remove("btn-warning")
-      }, 1/speed * 300)
-      console.log(audioButton)
+        audioButton.classList.remove("drum-active");
+      }, (1 / speed) * 300);
+      console.log(audioButton);
       audioTag.volume = volume;
       audioTag.currentTime = 0;
       audioTag.play();
-      index++
-    }, 1/speed * 300);
-    setTimeout(() => clearInterval(interval), 1/speed * 300 * recordArray.length)
+      index++;
+    }, (1 / speed) * 300);
+    setTimeout(
+      () => clearInterval(interval),
+      (1 / speed) * 300 * recordArray.length
+    );
   };
 
   const deleteLast = () => {
-    setRecording(recording.slice(0, recording.length - 1))
-  }
+    setRecording(recording.slice(0, recording.length - 1));
+  };
 
   return (
-    <div id="drum-machine" className="d-flex justify-content-center align-items-center bg-dark min-vh-100 text-dark">
-      <div id="display" className="w-50">
-        <h1 className="text-center">Drum Machine</h1>
-        <div className="d-flex justify-content-around flex-wrap">
-          {audioClips.map((clip) => {
-            return (
-              <Pad
-                key={clip.id}
-                clip={clip}
-                volume={volume}
-                setRecording={setRecording}
-              />
-            );
-          })}
+    <div id="drum-machine" className="bg-dark">
+      <h1 className="text-white">Drum Machine</h1>
+      <div className="containerMachine">
+        <div id="display">
+          <h2>let's play</h2>
         </div>
-        <h2>Volume: {Math.round(Number(volume) * 100)}%</h2>
-        <input
-          type="range"
-          onChange={(e) => setVolume(e.target.value)}
-          value={volume}
-          min="0"
-          max="1"
-          step="0.01"
-        />
-        <div>{recording}</div>
-        {recording && (
-          <>
-            <button onClick={playRecording} className="btn btn-primary">
-              Play
-            </button>
-            <button onClick={deleteLast} className="btn btn-danger"><i className="fa-solid fa-delete-left"></i></button>
-            <button onClick={() => setRecording("")} className="btn btn-danger">
-              Clear
-            </button>
-            <h2>Speed</h2>
+        <div className="pad">
+          <div className="volumeContainer">
+            <p className="volume">Volume: {Math.round(Number(volume) * 100)}%</p>
             <input
-            type="range"
-            onChange={(e) => setSpeed(e.target.value)}
-            value={speed}
-            min="0.7"
-            max="1.3"
-            step="0.01"
+              type="range"
+              className="range"
+              onChange={(e) => setVolume(e.target.value)}
+              value={volume}
+              min="0"
+              max="1"
+              step="0.01"
             />
-          </>
-        )}
+          </div>
+          <hr />
+          <div className="buttonsContainer">
+            {audioClips.map((clip) => {
+              return (
+                <Pad
+                  key={clip.id}
+                  clip={clip}
+                  volume={volume}
+                  setRecording={setRecording}
+                />
+              );
+            })}
+          </div>
+          {recording && <hr />}
+          <div className="recording">{recording}</div>
+          {recording && (
+            <>
+              <div className="controlsContainer">
+                <div className="buttonControlContainer">
+                  <div className="buttonBorder">
+                    <button onClick={playRecording}>
+                      Play
+                    </button>
+                  </div>
+                </div>
+                <div className="buttonControlContainer">
+                  <div className="buttonBorder">
+                    <button onClick={deleteLast}>
+                      <i className="fa-solid fa-delete-left"></i>
+                    </button>
+                  </div>
+                </div>
+                <div className="buttonControlContainer">
+                  <div className="buttonBorder">
+                    <button
+                      onClick={() => setRecording("")}
+                    >
+                      Clear
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="speedContainer">
+                <p className="txtSpeed">Speed</p>
+                <input
+                  type="range"
+                  className="range"
+                  onChange={(e) => setSpeed(e.target.value)}
+                  value={speed}
+                  min="0.7"
+                  max="1.3"
+                  step="0.01"
+                />
+              </div>
+            </>
+          )}
+        </div>
       </div>
+      <footer>
+        <p>Created by Nicolas Fontana</p>
+      </footer>
     </div>
   );
 };
@@ -174,13 +212,17 @@ const Pad = ({ clip, volume, setRecording }) => {
   };
 
   return (
-    <div
-      id={clip.keyCode}
-      onClick={playSound}
-      className={`btn btn-primary p-4 ${active && "btn-warning"} drum-pad`}
-    >
-      <audio id={clip.keyTrigger} className="clip" src={clip.url} />
-      {clip.keyTrigger}
+    <div className="buttonContainer">
+      <div className="buttonBorder">
+        <div
+          id={clip.keyCode}
+          onClick={playSound}
+          className={`${active && "drum-active"} drum-pad`}
+        >
+          <audio id={clip.keyTrigger} className="clip" src={clip.url} />
+          {clip.keyTrigger}
+        </div>
+      </div>
     </div>
   );
 };
